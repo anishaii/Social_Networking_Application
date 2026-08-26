e5c34d2b45014a0dc6c4b4c078c2a97f0d3905a5
Anisha <anishagc83@gmail.com>
2026-08-26 17:08:25 +0545

    recent changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import userRoutes from './routes/userRoute.js';
-import postRoutes from './routes/postRoute.js'
+import postRoutes from './routes/postRoute.js';
 import profileRoutes from './routes/profileRoute.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
